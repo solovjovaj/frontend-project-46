b5c72ca1efc62b906d4f82967b9cead6445c3c2f
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-02-16 14:45:56 +0700

    add gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+ import { Command } from 'commander/esm.mjs';
+ const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('-V, --version')
+  .helpOption('-h, --help');
+
+  program.parse();
+
