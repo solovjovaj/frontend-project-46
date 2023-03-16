502f7c4bc75275a5ebe2270414e197ff9ebc8bd8
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-03-16 18:01:28 +0300

    add changes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
  import { Command } from 'commander/esm.mjs';
- import genDiff from '../src/genDiff.js';
+ import genDiff from '../src/index.js';
  const program = new Command();
 
 program
