5ce0ffb2c81e2182ce37612d4b91c70ee0d1a2c2
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-03-22 14:02:17 +0300

    changed gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
- import { Command } from 'commander/esm.mjs';
- import genDiff from '../src/index.js';
- const program = new Command();
+
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
+const program = new Command();
 
 program
-  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-v, --version', 'output the version number')
-  .helpOption('-h, --help')
-  .option('-f, --format <type>', 'output format')
+  .description('Compares two configuration files and show a diference.')
   .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2))
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
-  program.parse();
+program.parse();
 
