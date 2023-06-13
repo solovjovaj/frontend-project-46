4376f6f7f71b6d68a8850c5ada4a7b84e9246932
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-06-13 15:22:12 +0200

    add stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,47 +1,44 @@
-import _ from 'lodash';
+const getCurrentIndent = (depth, intend = 4) => ' '.repeat(intend * depth - 2);
+const getClosingIndent = (depth, intend = 4) => ' '.repeat(intend * depth - intend);
 
-const makeIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(depth * spacesCount - 2);
-
-const valueFromation = (data, stylish, depth = 1) => {
-  if (!_.isObject(data)) {
-    return data;
-  }
-  const keys = Object.keys(data);
-  const result = keys.map((name) => {
-    const value = data[name];
-    return stylish({ name, value, type: 'unchanged' }, depth + 1);
-  });
-  return `{\n${result.join('\n')}\n  ${makeIndent(depth)}}`;
+const stringfy = (value, depth = 1) => {
+  const iter = (currentValue, depthIter) => {
+    const iterIndent = getCurrentIndent(depthIter);
+    if (typeof currentValue !== 'object' || currentValue === null) {
+      return `${currentValue}`;
+    }
+    const massValue = Object.entries(currentValue);
+    const lines = massValue.map(([key, val]) => `${iterIndent}  ${key}: ${iter(val, depthIter + 1)}`);
+    const result = ['{', ...lines, `${getClosingIndent(depthIter)}}`].join('\n');
+    return result;
+  };
+  return iter(value, depth);
 };
 
-const stylish = (diff, depth = 0) => {
-  const {
-    name, value, type, value1, value2, children,
-  } = diff;
+const getStylishFormat = (diffTree) => {
+  const iter = (diffObj, depth = 1) => {
+    const iterIndent = getCurrentIndent(depth);
+    const closingIndent = getClosingIndent(depth);
 
-  switch (type) {
-    case 'root': {
-      const resultLine = children.flatMap((child) => stylish(child, depth + 1));
-      return `{\n${resultLine.join('\n')}\n}`;
-    }
-    case 'nested': {
-      const resultLine = children.flatMap((child) => stylish(child, depth + 1));
-      return `${makeIndent(depth)}  ${name}: {\n${resultLine.join('\n')}\n${makeIndent(depth)}  }`;
-    }
-    case 'added':
-      return `${makeIndent(depth)}+ ${name}: ${valueFromation(value, stylish, depth)}`;
-    case 'deleted':
-      return `${makeIndent(depth)}- ${name}: ${valueFromation(value, stylish, depth)}`;
-    case 'unchanged':
-      return `${makeIndent(depth)}  ${name}: ${valueFromation(value, stylish, depth)}`;
-    case 'changed': {
-      const removed = `${makeIndent(depth)}- ${name}: ${valueFromation(value1, stylish, depth)}`;
-      const added = `${makeIndent(depth)}+ ${name}: ${valueFromation(value2, stylish, depth)}`;
-      return `${removed}\n${added}`;
-    }
-    default:
-      throw new Error(`Type: ${type} is undefined`);
-  }
+    const result = diffObj.flatMap((user) => {
+      switch (user.status) {
+        case 'nested':
+          return `${iterIndent}  ${user.key}: ${iter(user.children, depth + 1)}`;
+        case 'deleted':
+          return `${iterIndent}- ${user.key}: ${stringfy(user.value, depth + 1)}`;
+        case 'added':
+          return `${iterIndent}+ ${user.key}: ${stringfy(user.value, depth + 1)}`;
+        case 'changed':
+          return [`${iterIndent}- ${user.key}: ${stringfy(user.value1, depth + 1)}`,
+            `${iterIndent}+ ${user.key}: ${stringfy(user.value2, depth + 1)}`];
+        default:
+          return `${iterIndent}  ${user.key}: ${stringfy(user.value, depth + 1)}`;
+      }
+    });
+    return ['{', ...result, `${closingIndent}}`].join('\n');
+  };
+
+  return iter(diffTree);
 };
 
-export default stylish;
+export default getStylishFormat;
